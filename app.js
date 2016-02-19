792b2dbcb5f1426c161f2b0db0aea05422e36010
zakizak <zakizak2016@yahoo.com>
2016-02-19 21:03:48 +0700

    add files

---
+++ app.js
@@ -0,0 +1,72 @@
+// DEPENDENCIES
+var express = require('express');
+var swig = require('swig');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var helmet = require('helmet');
+var app = express();
+
+
+
+// SECURITY
+app.use(helmet());
+app.use(helmet.frameguard())
+app.disable('x-powered-by');
+
+
+// VIEW ENGINE SETUP
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+
+// MIDDLEWARE
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ROUTER
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+
+// ERROR HANDLER
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
